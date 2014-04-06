347695b3d47c30c952280d90ad0d56c923cec152
Horacio Duran <hduran@machinalis.com>
2014-04-06 17:18:03 -0300

    Added keywords to setup.py

--- ninja_ide.contrib.plugins.plugin_creator/setup.py
+++ ninja_ide.contrib.plugins.plugin_creator/setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='ninja_ide.contrib.plugins.plugin_creator',
       version='0.1',
+      keywords="ninja_ide plugin",
       namespace_packages=['ninja_ide', 'ninja_ide.contrib',
                           'ninja_ide.contrib.plugins'],
       packages=find_packages(),
